252006f35b2093a551515ab38213dddc70335322
Rochdi Benabdeljelil <rochdi92@hotmail.com>
2020-07-06 17:16:21 +0300

    Initial Commit

---
+++ Reciplease/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by rochdi ben abdeljelil on 06.07.2020.
+//  Copyright © 2020 rochdi ben abdeljelil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
